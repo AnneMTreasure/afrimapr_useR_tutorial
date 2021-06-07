12ea7c8c93a7b0451266483ab4cda376e6753ab9
AnneMTreasure <AnneMTreasure@users.noreply.github.com>
2021-06-07 12:32:50 +0200

    Add files via upload

---
+++ english/for_learners/userR_tutorial_code_session_two.R
@@ -0,0 +1,30 @@
+#### LOAD TUTORIAL IN VIEWER PANE ----
+source("english/for_instructors/scripts/viewerpane_html.R")
+viewerpane_html("english/for_learners/tutorials_pdf_html/userR_tutorial_session_two.html")
+
+
+
+### TO COMPLETE ONCE RMD FINALISED
+
+
+
+
+
+
+#### SECTION C: READ SPATIAL DATA FROM FILES
+
+# read files vector
+
+library(sf)
+filename1 <- system.file("extdata","africountries.shp", package="afrilearndata", mustWork=TRUE)
+myobject1 <- sf::st_read(filename1)
+
+# read files raster
+
+library(raster)
+filename2 <- system.file("extdata","afripop2020.tif", package="afrilearndata", mustWork=TRUE)
+myobject2 <- raster::raster(filename2)
+
+
+
+
